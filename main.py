3504e224de16f3be9f147eea4740c68c1066f79b
Ferenc Marcso <marcso.ferenc@gmail.com>
2021-05-28 22:32:38 +0200

    Prepare Entry Point For Dockerization

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def main():
     parser = argparse.ArgumentParser(description='Run Selenium Based Test Cases')
     parser.add_argument('--browser', default='chrome',
                         help='Browser to run')
+    parser.add_argument('--selenium', default='http://127.0.0.1:4444',
+                        help='Selenium hub URL')
 
     args = parser.parse_args()
     browser = args.browser
@@ -20,7 +22,10 @@ def main():
         print('Invalid browser argument!')
         print('chrome option will be applied')
 
-    robot.run_cli(['-d', f'Results/{timestamp}', '-v', f'browser:{browser}', 'src/Tests'])
+    robot.run_cli(['-d', f'Results/{timestamp}',
+                   '-v', f'browser:{browser}',
+                   '-v', f'remote_url:{args.selenium}',
+                   'src/Tests'])
 
 
 if __name__ == '__main__':
